7db515b4c8b6fc6f8c3aa9d59c3faa1c5a3602e3
Andre Girao <andrelgirao22@hotmail.com>
2018-07-04 13:11:26 -0300

    criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -31,9 +31,7 @@ export class CategoriasPage {
     console.log('ionViewDidLoad CategoriasPage');
     this.categoriaService.findAll().subscribe(response => {
       this.items = response
-    }, error => {
-      console.log(error)
-    })
+    }, error => {})
   }
 
 }
